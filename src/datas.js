57228ca1f9ab9e47b3e326481a878e7501b8e643
Beyza Arslanturk <bnur.arslanturk@gmail.com>
2024-02-21 11:46:49 +0300

    new commit

--- src/datas.js
+++ src/datas.js
@@ -1,255 +1,240 @@
 // skills
-import html from "./assets/skills/html.png"
-import css from "./assets/skills/css.png"
-import javascript from "./assets/skills/javascript.png"
-import reactImage from "./assets/skills/react.png"
-import scss from "./assets/skills/scss.png"
-import bootstrap from "./assets/skills/bootstrap-600x600.jpg"
-import tailwind from "./assets/skills/tailwind.png"
+import html from "./assets/skills/html.png";
+import css from "./assets/skills/css.png";
+import javascript from "./assets/skills/javascript.png";
+import reactImage from "./assets/skills/react.png";
+import scss from "./assets/skills/scss.png";
+import bootstrap from "./assets/skills/bootstrap-600x600.jpg";
+import tailwind from "./assets/skills/tailwind.png";
 
 // projects
-import ecommerce from "./assets/projects/ecommerce-app.png"
-import books from "./assets/projects/react-books-app.png"
-import cart from "./assets/projects/cart-app.png"
-import movies from "./assets/projects/movie-app.png"
-import googlebooks from "./assets/projects/google-books-app.png"
-import recipe from "./assets/projects/recipe-app.png"
-import faketweet from "./assets/projects/fake-tweet-app.png"
-import weather from "./assets/projects/weather_api.png"
-import spendmoney from "./assets/projects/money-spending-app.png"
-import todo from "./assets/projects/react-todo.png"
-import dictionary from "./assets/projects/dictionary-app.png"
+import ecommerce from "./assets/projects/ecommerce-app.png";
+import books from "./assets/projects/react-books-app.png";
+import cart from "./assets/projects/cart-app.png";
+import movies from "./assets/projects/movie-app.png";
+import googlebooks from "./assets/projects/google-books-app.png";
+import recipe from "./assets/projects/recipe-app.png";
+import faketweet from "./assets/projects/fake-tweet-app.png";
+import weather from "./assets/projects/weather_api.png";
+import spendmoney from "./assets/projects/money-spending-app.png";
+import todo from "./assets/projects/react-todo.png";
+import dictionary from "./assets/projects/dictionary-app.png";
 
+import bmicalculator from "./assets/projects/bmi-calculator.png";
+import loancalculator from "./assets/projects/loancalculator.png";
+import numberguesser from "./assets/projects/numberguesser.png";
 
-import bmicalculator from "./assets/projects/bmi-calculator.png"
-import loancalculator from "./assets/projects/loancalculator.png"
-import numberguesser from "./assets/projects/numberguesser.png"
-
-import gainio from "./assets/projects/gainio.png"
-import faba from "./assets/projects/faba.png"
-import paradis from "./assets/projects/paradis.png"
-import rockie from "./assets/projects/rockie.png"
-
-
+import gainio from "./assets/projects/gainio.png";
+import faba from "./assets/projects/faba.png";
+import paradis from "./assets/projects/paradis.png";
+import rockie from "./assets/projects/rockie.png";
 
 export const projectsReact = [
+  {
+    id: 1,
+    src: ecommerce,
+    title: "E-commerce App",
+    code: "https://github.com/beyzaarslanturk/react-ecommerce-app",
+    display: "https://beyzaarslanturk-react-ecommerce-app.netlify.app/",
+  },
+  {
+    id: 2,
+    src: books,
+    title: "BookList App",
+    code: "https://github.com/beyzaarslanturk/React-Books-App",
+    display: "https://beyzaarslanturk-react-books-app.netlify.app/",
+  },
 
-    {
-        id: 1,
-        src: ecommerce,
-        title: "E-commerce App",
-        code: "https://github.com/beyzaarslanturk/react-ecommerce-app",
-        display: "https://beyzaarslanturk-react-ecommerce-app.netlify.app/"
-    },
-    {
-        id: 2,
-        src: books,
-        title: "BookList App",
-        code: "https://github.com/beyzaarslanturk/React-Books-App",
-        display: "https://beyzaarslanturk-react-books-app.netlify.app/"
-    },
-
-    {
-        id: 3,
-        src: cart,
-        title: "Shopping Cart ",
-        code: "https://github.com/beyzaarslanturk/React-Shopping-Cart-App",
-        display: "https://beyzaarslanturk-react-shopping-cart.surge.sh/"
-    },
-    {
-        id: 4,
-        src: movies,
-        title: "Movies App",
-        code: "https://github.com/beyzaarslanturk/React-Movie-App",
-        display: "https://beyzaarslanturk-react-movie-app.surge.sh/"
-    },
-    {
-        id: 5,
-        src: googlebooks,
-        title: "Google Books App",
-        code: "https://github.com/beyzaarslanturk/React-Google-Books-App",
-        display: "https://beyzaarslanturk-google-books-app.surge.sh/"
-    },
-    {
-        id: 6,
-        src: recipe,
-        title: "Recipe App",
-        code: "https://github.com/beyzaarslanturk/React-Recipe-App",
-        display: "http://beyzaarslanturk-react-recipe-app.surge.sh/"
-    },
-    {
-        id: 7,
-        src: faketweet,
-        title: "FakeTweet Generator",
-        code: "https://github.com/beyzaarslanturk/React-Fake-Tweet-Generator",
-        display: "https://beyzarslanturk-fake-tweet.surge.sh/"
-    },
-    {
-        id: 8,
-        src: spendmoney,
-        title: "Spend Money App",
-        code: "https://github.com/beyzaarslanturk/React-MoneySpendingApp",
-        display: "https://beyzaarslanturk-react-moneyspending.netlify.app/"
-    },
-    {
-        id: 9,
-        src: dictionary,
-        title: "Dictionary App",
-        code: "https://github.com/anilcosarss/Javascript-Dictionary-API",
-        display: "https://raw.githack.com/anilcosarss/Javascript-Dictionary-API/main/index.html"
-    },
-    {
-        id: 10,
-        src: weather,
-        title: "Weather App",
-        code: "https://github.com/beyzaarslanturk/React-Weather-API",
-        display: "https://beyzaarslanturk-react-weather-api.netlify.app/"
-    },
-    {
-        id: 11,
-        src: todo,
-        title: "Todo App",
-        code: "https://github.com/beyzaarslanturk/React-Todo",
-        display: "https://beyzaarslanturk-react-todo.netlify.app/"
-    },
-    
-       
-]
-
+  {
+    id: 3,
+    src: cart,
+    title: "Shopping Cart ",
+    code: "https://github.com/beyzaarslanturk/React-Shopping-Cart-App",
+    display: "https://beyzaarslanturk-react-shopping-cart.surge.sh/",
+  },
+  {
+    id: 4,
+    src: movies,
+    title: "Movies App",
+    code: "https://github.com/beyzaarslanturk/React-Movie-App",
+    display: "https://beyzaarslanturk-react-movie-app.surge.sh/",
+  },
+  {
+    id: 5,
+    src: googlebooks,
+    title: "Google Books App",
+    code: "https://github.com/beyzaarslanturk/React-Google-Books-App",
+    display: "https://beyzaarslanturk-google-books-app.surge.sh/",
+  },
+  {
+    id: 6,
+    src: recipe,
+    title: "Recipe App",
+    code: "https://github.com/beyzaarslanturk/React-Recipe-App",
+    display: "http://beyzaarslanturk-react-recipe-app.surge.sh/",
+  },
+  {
+    id: 7,
+    src: faketweet,
+    title: "FakeTweet Generator",
+    code: "https://github.com/beyzaarslanturk/React-Fake-Tweet-Generator",
+    display: "https://beyzarslanturk-fake-tweet.surge.sh/",
+  },
+  {
+    id: 8,
+    src: spendmoney,
+    title: "Spend Money App",
+    code: "https://github.com/beyzaarslanturk/React-MoneySpendingApp",
+    display: "https://beyzaarslanturk-react-moneyspending.netlify.app/",
+  },
+  {
+    id: 9,
+    src: dictionary,
+    title: "Dictionary App",
+    code: "https://github.com/beyzaarslanturk/React-Dictionary-App",
+    display: "https://beyzaarslanturk-react-dictionary-app.netlify.app/",
+  },
+  {
+    id: 10,
+    src: weather,
+    title: "Weather App",
+    code: "https://github.com/beyzaarslanturk/React-Weather-API",
+    display: "https://beyzaarslanturk-react-weather-api.netlify.app/",
+  },
+  {
+    id: 11,
+    src: todo,
+    title: "Todo App",
+    code: "https://github.com/beyzaarslanturk/React-Todo",
+    display: "https://beyzaarslanturk-react-todo.netlify.app/",
+  },
+];
 
 export const projectsJs = [
-    {
-        id: 1,
-        src: bmicalculator,
-        title: "BMI Calculator",
-        code: "https://github.com/beyzaarslanturk/Javascript-BMICalculator",
-        display: "https://beyzaarslanturk-bmicalculator.netlify.app/"
-    },
-    {
-        id: 2,
-        src: loancalculator,
-        title: "Loan Calculator",
-        code: "https://github.com/beyzaarslanturk/Javascript-Loan-Calculator",
-        display: "https://master--beyzaarslanturk-loancalculator.netlify.app/"
-    },    
-    {
-        id: 3,
-        src: numberguesser,
-        title: "NumberGuesser Game",
-        code: "https://github.com/beyzaarslanturk/Javascript-NumberGuesser",
-        display: "https://beyzaarslanturk-numberguesser.netlify.app/"
-    },
-      
-]
-
+  {
+    id: 1,
+    src: bmicalculator,
+    title: "BMI Calculator",
+    code: "https://github.com/beyzaarslanturk/Javascript-BMICalculator",
+    display: "https://beyzaarslanturk-bmicalculator.netlify.app/",
+  },
+  {
+    id: 2,
+    src: loancalculator,
+    title: "Loan Calculator",
+    code: "https://github.com/beyzaarslanturk/Javascript-Loan-Calculator",
+    display: "https://master--beyzaarslanturk-loancalculator.netlify.app/",
+  },
+  {
+    id: 3,
+    src: numberguesser,
+    title: "NumberGuesser Game",
+    code: "https://github.com/beyzaarslanturk/Javascript-NumberGuesser",
+    display: "https://beyzaarslanturk-numberguesser.netlify.app/",
+  },
+];
 
 export const projectsCss = [
-    
-    {
-        id: 1,
-        src: gainio,
-        title: "Gainio",
-        code: "https://github.com/beyzaarslanturk/Gainio",
-        display: "https://raw.githack.com/beyzaarslanturk/Gainio/master/index.html"
-    },
-    {
-        id: 2,
-        src: faba,
-        title: "Faba Crypto",
-        code: "https://github.com/beyzaarslanturk/Faba",
-        display: "https://raw.githack.com/beyzaarslanturk/Faba/master/index.html"
-    },
-    {
-        id: 3,
-        src: paradis,
-        title: "Paradis",
-        code: "https://github.com/beyzaarslanturk/Paradis",
-        display: "https://raw.githack.com/beyzaarslanturk/Paradis/master/index.html"
-    },
-    {
-        id: 4,
-        src: rockie,
-        title: "Rockie Crypto",
-        code: "https://github.com/beyzaarslanturk/Rockie",
-        display: "https://raw.githack.com/beyzaarslanturk/Rockie/master/index.html"
-    },
-]
-
-
-
-
-
-
+  {
+    id: 1,
+    src: gainio,
+    title: "Gainio",
+    code: "https://github.com/beyzaarslanturk/Gainio",
+    display: "https://raw.githack.com/beyzaarslanturk/Gainio/master/index.html",
+  },
+  {
+    id: 2,
+    src: faba,
+    title: "Faba Crypto",
+    code: "https://github.com/beyzaarslanturk/Faba",
+    display: "https://raw.githack.com/beyzaarslanturk/Faba/master/index.html",
+  },
+  {
+    id: 3,
+    src: paradis,
+    title: "Paradis",
+    code: "https://github.com/beyzaarslanturk/Paradis",
+    display:
+      "https://raw.githack.com/beyzaarslanturk/Paradis/master/index.html",
+  },
+  {
+    id: 4,
+    src: rockie,
+    title: "Rockie Crypto",
+    code: "https://github.com/beyzaarslanturk/Rockie",
+    display: "https://raw.githack.com/beyzaarslanturk/Rockie/master/index.html",
+  },
+];
 
 // navlinks
 export const links = [
-    {
-        id: 1,
-        link: "/",
-        name: "Home"
-    },
+  {
+    id: 1,
+    link: "/",
+    name: "Home",
+  },
 
-    {
-        id: 2,
-        link: "/skills",
-        name: "Skills"
-    },
+  {
+    id: 2,
+    link: "/skills",
+    name: "Skills",
+  },
 
-    {
-        id: 3,
-        link: "/projects",
-        name: "Projects"
-    },
+  {
+    id: 3,
+    link: "/projects",
+    name: "Projects",
+  },
 
-    {
-        id: 4,
-        link: "/contact",
-        name: "Contact"
-    }
-]
+  {
+    id: 4,
+    link: "/contact",
+    name: "Contact",
+  },
+];
 
 export const skills = [
-    {
-        id: 1,
-        src: html,
-        title: "HTML",
-        style: "shadow-orange-500"
-    },
-    {
-        id: 2,
-        src: css,
-        title: "CSS",
-        style: "shadow-blue-500"
-    },
-    {
-        id: 3,
-        src: javascript,
-        title: "JavaScript",
-        style: "shadow-yellow-500"
-    },
-    {
-        id: 4,
-        src: reactImage,
-        title: "React",
-        style: "shadow-blue-600"
-    },
-    {
-        id: 5,
-        src: scss,
-        title: "SCSS",
-        style: "shadow-pink-500"
-    },
-    {
-        id: 6,
-        src: bootstrap,
-        title: "Bootstrap",
-        style: "shadow-sky-400"
-    },
+  {
+    id: 1,
+    src: html,
+    title: "HTML",
+    style: "shadow-orange-500",
+  },
+  {
+    id: 2,
+    src: css,
+    title: "CSS",
+    style: "shadow-blue-500",
+  },
+  {
+    id: 3,
+    src: javascript,
+    title: "JavaScript",
+    style: "shadow-yellow-500",
+  },
+  {
+    id: 4,
+    src: reactImage,
+    title: "React",
+    style: "shadow-blue-600",
+  },
+  {
+    id: 5,
+    src: scss,
+    title: "SCSS",
+    style: "shadow-pink-500",
+  },
+  {
+    id: 6,
+    src: bootstrap,
+    title: "Bootstrap",
+    style: "shadow-sky-400",
+  },
 
-    {
-        id: 7,
-        src: tailwind,
-        title: "Tailwind",
-        style: "shadow-sky-300"
-    }
-]
+  {
+    id: 7,
+    src: tailwind,
+    title: "Tailwind",
+    style: "shadow-sky-300",
+  },
+];
